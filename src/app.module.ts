650c6c4ea4c7f99b21d22b0f54e0b09835de3eb2
Walter Restifo <walterrestifo93@gmail.com>
2025-11-18 20:47:52 +0100

    remove scheduler 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { GameSyncModule } from './game-sync/game-sync.module';
 import { GameManagerService } from './services/game-manager/game-manager/game-manager.service';
 import { SessionsManagerService } from './services/sessions-manager/sessions-manager.service';
-import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [GameSyncModule, ScheduleModule.forRoot()],
+  imports: [GameSyncModule],
   controllers: [],
   providers: [
     GameManagerService,
