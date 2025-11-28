ba33201e79a31efdc9d75b536ff214412c41ddf2
Walter Restifo <walterrestifo93@gmail.com>
2025-11-28 08:48:23 +0100

    add environment variables because of cors origin problem

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GameSyncModule } from './game-sync/game-sync.module';
 import { GameManagerService } from './services/game-manager/game-manager/game-manager.service';
 import { SessionsManagerService } from './services/sessions-manager/sessions-manager.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GameSyncModule],
+  imports: [GameSyncModule, ConfigModule.forRoot()],
   controllers: [],
   providers: [
     GameManagerService,
